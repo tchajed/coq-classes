a6ec630fd28ba0720f909aa5e5f964e3c8850328
Tej Chajed <tchajed@mit.edu>
2019-01-16 13:26:15 -0500

    Add reasonably efficient string equality instance

--- src/EqualDec.v
+++ src/EqualDec.v
@@ -1,4 +1,5 @@
 From Coq Require Import ProofIrrelevance.
+From Coq Require Import String.
 
 Class EqualDec A :=
   equal : forall x y : A, { x = y } + { x <> y }.
@@ -16,7 +17,7 @@ Instance unit_equal_dec : EqualDec unit :=
                 | tt, tt => eq_refl
                 end).
 
-Hint Extern 3 (EqualDec _) => hnf; decide equality : typeclass_instances.
+Hint Extern 2 (EqualDec _) => hnf; decide equality : typeclass_instances.
 
 Instance nat_equal_dec : EqualDec nat := _.
 Instance bool_equal_dec : EqualDec bool := _.
@@ -31,3 +32,51 @@ Proof.
   - intro.
     inversion H; congruence.
 Qed.
+
+(* TODO: this could be much more efficient if it first did all the computation
+and then produced the proof, opaquely *)
+
+Definition ascii_cmp (x y: Ascii.ascii) : bool :=
+  match x, y with
+  | Ascii.Ascii x0 x1 x2 x3 x4 x5 x6 x7,
+    Ascii.Ascii y0 y1 y2 y3 y4 y5 y6 y7 =>
+    Bool.eqb x0 y0 &&
+             Bool.eqb x1 y1 &&
+             Bool.eqb x2 y2 &&
+             Bool.eqb x3 y3 &&
+             Bool.eqb x4 y4 &&
+             Bool.eqb x5 y5 &&
+             Bool.eqb x6 y6 &&
+             Bool.eqb x7 y7
+  end.
+
+Ltac noteq := inversion 1; congruence.
+
+Theorem ascii_cmp_ok : forall x y,
+    if ascii_cmp x y then x = y else x <> y.
+Proof.
+  destruct x as [x0 x1 x2 x3 x4 x5 x6 x7], y as [y0 y1 y2 y3 y4 y5 y6 y7];
+    simpl.
+  destruct (Bool.eqb_spec x0 y0); [ | noteq ].
+  destruct (Bool.eqb_spec x1 y1); [ | noteq ].
+  destruct (Bool.eqb_spec x2 y2); [ | noteq ].
+  destruct (Bool.eqb_spec x3 y3); [ | noteq ].
+  destruct (Bool.eqb_spec x4 y4); [ | noteq ].
+  destruct (Bool.eqb_spec x5 y5); [ | noteq ].
+  destruct (Bool.eqb_spec x6 y6); [ | noteq ].
+  destruct (Bool.eqb_spec x7 y7); [ | noteq ].
+  simpl; f_equal; auto.
+Qed.
+
+Instance ascii_eq_dec : EqualDec Ascii.ascii.
+Proof.
+  hnf; intros.
+  pose proof (ascii_cmp_ok x y).
+  destruct (ascii_cmp x y); auto.
+Defined.
+
+Instance string_eq_dec : EqualDec string.
+Proof.
+  hnf; decide equality.
+  destruct (equal a a0); auto.
+Defined.
